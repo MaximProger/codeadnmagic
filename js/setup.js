3fb9e49fa360b540bdaf83551727a8588b66d2a7
Maksim Koshman <maksimkoshman090@gmail.com>
2020-12-16 21:29:30 +0300

    Добавлены события клика для открытия/закрытия модлаьного окна. Валидация поля имени. Смена облика волшебника, по клику на его предметы.

--- js/setup.js
+++ js/setup.js
@@ -1,11 +1,17 @@
-// Возращает случайное число в диапазоне
+// Return random number in range
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 }
 
-document.querySelector(".setup").classList.remove("hidden");
+// Return Uniq Array Value
+function getArrayUniqValue(array, currValue) {
+  let uniqValue = array.filter((item) => item != currValue);
+  let arrayValue = uniqValue[getRandomIntInclusive(0, uniqValue.length - 1)];
+  return arrayValue;
+}
+
 document.querySelector(".setup-similar").classList.remove("hidden");
 
 // Data
@@ -99,3 +105,95 @@ for (let i = 0; i < wizards.length; i++) {
 }
 
 setupSimilarList.appendChild(fragment);
+
+// Setup Popup
+const setup = document.querySelector(".setup");
+const setupClose = document.querySelector(".setup-close");
+const setupOpen = document.querySelector(".setup-open");
+const setupOpenIcon = document.querySelector(".setup-open-icon");
+
+let openPopup = function () {
+  setup.classList.remove("hidden");
+};
+
+let closePopup = function () {
+  setup.classList.add("hidden");
+};
+
+setupOpen.addEventListener("click", function () {
+  openPopup();
+});
+
+setupOpenIcon.addEventListener("keydown", function (evt) {
+  if (evt.keyCode == 13) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener("click", function () {
+  closePopup();
+});
+
+setupClose.addEventListener("keydown", function (evt) {
+  if (evt.keyCode == 13 && !setup.classList.contains("hidden")) {
+    closePopup();
+  }
+});
+
+document.addEventListener("keydown", function (evt) {
+  if (evt.keyCode == 27 && evt.target.className != "setup-user-name") {
+    closePopup();
+  }
+});
+
+// Submit
+const setupSubmitBtn = document.querySelector(".setup-submit");
+const setupWizardForm = document.querySelector(".setup-wizard-form");
+
+setupSubmitBtn.addEventListener("keydown", function (evt) {
+  if (evt.keyCode == 13 && !setup.classList.contains("hidden")) {
+    setupWizardForm.submit();
+  }
+});
+
+// Switch colors
+const setupWizardCoat = document.querySelector(".setup-wizard .wizard-coat");
+
+setupWizardCoat.addEventListener("click", function () {
+  let getCoatColor = getArrayUniqValue(coatColors, this.style.fill);
+  this.style.fill = getCoatColor;
+});
+
+// Switch eyes
+const setupWizardEye = document.querySelector(".setup-wizard .wizard-eyes");
+
+setupWizardEye.addEventListener("click", function () {
+  let getEyeColor = getArrayUniqValue(eyesColors, this.style.fill);
+  this.style.fill = getEyeColor;
+});
+
+// Switch fireball
+const fireballColor = ["#ee4830", "#30a8ee", "#5ce6c0", "#e848d5", "#e6e848"];
+const setupWizardFireball = document.querySelector(".setup-fireball-wrap");
+
+setupWizardFireball.addEventListener("click", function () {
+  let getFireballColor = getArrayUniqValue(
+    fireballColor,
+    this.style.background
+  );
+  this.style.background = getFireballColor;
+});
+
+// Validation Name
+const setupUserName = document.querySelector(".setup-user-name");
+
+setupUserName.addEventListener("input", function (evt) {
+  let target = evt.target;
+  if (target.value.length < 2) {
+    target.setCustomValidity("Имя должно состоять минимум из 2-х символов");
+  } else if (target.value.length > 25) {
+    target.setCustomValidity("Имя должно состоять максимум из 25-ти символов");
+  } else {
+    target.setCustomValidity("");
+  }
+});
